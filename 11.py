e8f003370ae10a739b530f2019fdca28a0afe191
jjw9728 <pon04176@naver.com>
2024-11-29 13:33:07 +0900

    practice

---
+++ 11.py
@@ -0,0 +1,11 @@
+# print("hello, world!!")
+# print("hello",",","world!!", sep=" ")
+# print("안녕하세요", end=" ")
+# print("조재원입니다")
+
+# 실습 1 
+i = 30 // 4
+j = 30 % 4
+
+print("빵의 개수 =", i)
+print("남은 빵의 개수 =", j)
