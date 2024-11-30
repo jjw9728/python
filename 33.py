89cecd2f9478cc2cd970bd322d524c74923bc1ab
jjw9728 <pon04176@naver.com>
2024-11-30 13:50:08 +0900

    33

---
+++ 33.py
@@ -0,0 +1,68 @@
+# print("| \_/ |")
+# print("| q p |    /}")
+# print('(  0  )"""\\')
+# print('|"^"        |')
+# print('| |_/=\\\__ |')
+
+# name = str(input("이름을 입력하세요."))
+# ages = int(input("나이를 입력하세요."))
+# print("안녕하세요! ", name, "님", " (", ages, "세)", sep="")
+
+# name = str(input("이름을 입력하세요."))
+# ages = int(input("태어난 년도를 입력하세요."))
+# year = int(input("올해 년도를 입력하세요."))
+# n = year - ages + 1
+# print("올해는 ", year, "년",", ", name, "님의 나이는 ", n, "세 입니다", sep="")
+
+# a = []
+# b = [1, 2, 3, 4]
+# c = ["장원영", "안유진"]
+# d = [1, "아이브"]
+
+# print(len(c))
+# print(c[0])
+# print(c[1])
+
+# c[0] = "카리나"
+# print(c)
+# del c[0]
+# print(c)
+# c.append("조재원")
+# print(c)
+
+# b = [1, 2, 3, 4]
+# print(b[-3])
+# print(type(b))
+
+# seasons = ["봄", "여름", "가을", "겨울"]
+# print(seasons[0:1])
+# print(seasons[0:2])
+# print(seasons[:2])
+# print(seasons[1:])
+# print(seasons[::-1])
+
+# seasons = ["봄", "여름", "가을", "겨울", [1, 2, 3, 4]]
+# print(seasons[-1][0])
+
+# a = [1, 2, 3, 4, 5 ,6 ,7, 8, 9, 10]
+# even = a[1::2]
+# odd = a[0::2]
+# print(even)
+# print(odd)
+
+# num = [3, 1, 5, 2]
+# num.sort()
+# print(num)
+
+# b = ["a","k","s","e"]
+# b.sort()
+# print(b)
+
+rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+print(rainbow[2])
+rainbow.sort()
+print(rainbow)
+rainbow.append("cyan")
+print(rainbow)
+del rainbow[3:7]
+print(rainbow)
