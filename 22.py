cfb299a09c8bcc416d12140eeee6fa9747651b99
jjw9728 <pon04176@naver.com>
2024-11-30 11:08:37 +0900

    22

---
+++ 22.py
@@ -0,0 +1,42 @@
+import math
+
+# print(3//2)
+# print(3.25//2)
+# print(3%2)
+# print(3.25%2)
+# print(2**3)
+# print(2**10)
+# print(math.sqrt(16))
+
+# 실습 1
+# print("빵의 개수 =", 30 // 4)
+# print("남은 빵의 개수 =", 30 % 4)
+
+# a=1
+# a+=3
+# print(a)
+
+# print('안녕'+'반가워')
+# print('안녕'*10)
+
+# song = input("너의 최애 노래는?")
+# print(song)
+# print(type(song))
+
+# a = input('1 + 1 = ?')
+# print(int(a)*22)
+
+# a= int(input())
+# b= a%2
+
+# if a==0:
+#     print("n is zero")
+# if b==1:
+#     print("n is odd")
+# else:
+#     print("n is even")
+
+# a = 2
+# print(str(2)*10)
+# print(str(2)+"입니다")
+
