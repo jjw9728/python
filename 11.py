7ef69acd18f396cc4ee5c24762a3a55bcd0810f4
jjw9728 <pon04176@naver.com>
2024-11-30 11:08:19 +0900

    11

--- 11.py
+++ 11.py
@@ -8,5 +8,25 @@
 
 # print("빵의 개수 =", i)
 # print("남은 빵의 개수 =", j)
-real = "배고파"
-print(real)
+# real = "배고파"
+# print(f"나는 {real}")
+
+# print(type(77))
+# print(type(91.5))
+# print(type("77"))
+ 
+# number = 10
+# b_num = 0b1010
+# h_num = 0xA
+
+# print(number)
+# print(b_num)
+# print(h_num)
+# print(bin(10))
+# print(hex(10))
+
+# print(ord('0'))
+# print(ord('A'))
+# print(chr(48))
+# print(chr(65))
+
